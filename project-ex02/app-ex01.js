8f7a3f7f9a142096a754dba5a619c4fc23ff898c
yoon-ji-kim <yoonjigim0.0@gmail.com>
2023-03-03 13:57:55 +0900

    node #2

---
+++ project-ex02/app-ex01.js
@@ -0,0 +1,10 @@
+/**
+ *  douzone-math 파일 모듈 테스트
+ *  - 하나의 프로젝트 안 모듈 간 export/import 할 때 방식
+ */
+
+var math = require('../douzone-math/index');
+
+console.log(math.PI);
+console.log(math.max(150, 200, 30 ,40 ,50));
+console.log(math.min(150, 200, 30 ,40 ,50));
